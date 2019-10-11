29f654862e75fc4b7b907bf0596322729ef1ec24
Guilherme Aquino <guilherme.pl.aquino@gmail.com>
2019-10-10 22:15:30 -0300

    Acrescentado conta de vezes para dois valores

--- src/git_course/Program.java
+++ src/git_course/Program.java
@@ -1,12 +1,22 @@
 package git_course;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Bom dia!");
 		System.out.println("Olá");
 		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int x = n * m;
+		
+		System.out.println(x);
+		
+		sc.close();
 	}
 
 }
